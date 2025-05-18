d37a42cfee794e931182466bac3c1f3838eab9e2
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-05-18 16:27:24 +0700

    Footer added, padding adjusted

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import emailjs from '@emailjs/browser';
+import Footer from "../components/Footer";
 
 const ContactPage = () => {
   const [isClicked, setIsClicked] = useState(false);
@@ -62,66 +63,73 @@ const ContactPage = () => {
   }, [status]);
 
   return(
-    <main className="w-full h-full font-inter">
-      <div className="flex justify-center items-center px-20 pb-20 gap-2 fixed h-full w-full">
-        <div className="container h-[80dvh] gap-2 rounded-xl bg-linear-to-br from-brick-200 via-pink-200 to-sky-200 flex justify-between items-center px-40">
+    <>
+      <main className="w-full h-full font-inter">
+        <div className="flex justify-center items-center px-20 py-15 gap-2 h-full w-full">
+          
+          {/* Container */}
+          <div className="container h-[80dvh] gap-2 rounded-xl bg-linear-to-br from-brick-200 via-pink-200 to-sky-200 flex justify-between items-center px-40">
+            
+            {/* Contents */}
+            <div className="flex flex-col gap-6">
+              <h2 className="text-6xl text-word-blue font-playwrite">Contact Us</h2>
+              <p className="text-gray-blue w-[50dvh] leading-relaxed text-md">We accept any question or feedback from you! You also can directly message us on our social media.</p>
+            </div>
 
-          <div className="flex flex-col gap-6">
-            <h2 className="text-6xl text-word-blue font-playwrite">Contact Us</h2>
-            <p className="text-gray-blue w-[50dvh] leading-relaxed text-md">We accept any question or feedback from you! You also can directly message us on our social media.</p>
+            {/* Form */}
+            <div className="flex flex-col gap-2 w-1/2">
+              <form className="space-y-6 py-4" onSubmit={handleSubmit}>
+                <input 
+                  type="text"
+                  name="bot_field"
+                  style={{ display: 'none' }}
+                  tabIndex={-1}
+                />
+                <input 
+                  type="text" 
+                  id="name"
+                  placeholder="Your name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-2 py-3 rounded-md bg-gray-100/50 border-2 border-gray-blue/20 text-[#2a3e47] focus:outline-none focus:ring-2 focus:ring-word-orange"
+                  required
+                />
+                <input 
+                  type="email" 
+                  id="email"
+                  placeholder="Email address"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-2 py-3 rounded-md text-[#2a3e47] bg-gray-100/50 border-2 border-gray-blue/20 focus:outline-none focus:ring-2 focus:ring-word-orange"
+                  required
+                />
+                <textarea 
+                  id="message"
+                  placeholder="Your messages"
+                  rows="4"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="w-full px-2 py-1 rounded-md text-[#2a3e47] bg-gray-100/50 border-2 border-gray-blue/20 focus:outline-none focus:ring-2 focus:ring-word-orange"
+                  required
+                />
+                <button
+                  type="submit"
+                  className={`submit-btn cursor-pointer bg-word-blue/70 text-white rounded-2xl shadow-xl px-6 py-2
+                    hover:bg-word-blue transition-all duration-200 ease-in-out
+                    ${isClicked ? 'scale-95' : ''}`}
+                  disabled={isClicked}
+                >
+                  {isClicked ? "Sending...": "Send Message"}
+                </button>
+              </form>
+            </div>
+            
           </div>
-          
-          <div className="flex flex-col gap-2 w-1/2">
-            <form className="space-y-6 py-4" onSubmit={handleSubmit}>
-              <input 
-                type="text"
-                name="bot_field"
-                style={{ display: 'none' }}
-                tabIndex={-1}
-              />
-              <input 
-                type="text" 
-                id="name"
-                placeholder="Your name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full px-2 py-3 rounded-md bg-gray-100/50 border-2 border-gray-blue/20 text-[#2a3e47] focus:outline-none focus:ring-2 focus:ring-word-orange"
-                required
-              />
-              <input 
-                type="email" 
-                id="email"
-                placeholder="Email address"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full px-2 py-3 rounded-md text-[#2a3e47] bg-gray-100/50 border-2 border-gray-blue/20 focus:outline-none focus:ring-2 focus:ring-word-orange"
-                required
-              />
-              <textarea 
-                id="message"
-                placeholder="Your messages"
-                rows="4"
-                value={formData.message}
-                onChange={handleChange}
-                className="w-full px-2 py-1 rounded-md text-[#2a3e47] bg-gray-100/50 border-2 border-gray-blue/20 focus:outline-none focus:ring-2 focus:ring-word-orange"
-                required
-              />
-              <button
-                type="submit"
-                className={`submit-btn cursor-pointer bg-word-blue/70 text-white rounded-2xl shadow-xl px-6 py-2
-                  hover:bg-word-blue transition-all duration-200 ease-in-out
-                  ${isClicked ? 'scale-95' : ''}`}
-                disabled={isClicked}
-              >
-                {isClicked ? "Sending...": "Send Message"}
-              </button>
-            </form>
-          </div>
-          
+
         </div>
-
-      </div>
-    </main>
+      </main>
+      <Footer />
+    </>
   )
 }
 
