9ff6ad657b3bb8e8e042ef6edf03175e85571383
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-05-19 15:46:31 +0700

    Fix scroll to top when changing pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <ScrollToTop />
     <StrictMode>
       <App />
     </StrictMode>
